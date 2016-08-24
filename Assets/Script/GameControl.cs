388a4a5b4e1ca1f3e3fff6d00378843f201c66a1
Sniper Liu <lwt1231234@gmail.com>
2016-08-25 00:02:03 +0800

    2016.08.24
    1.增加了分配角色的功能
    2.增加了查看玩家角色功能
    3.修复了一个设置界面会触发玩家点击操作的问题

--- Assets/Script/GameControl.cs
+++ Assets/Script/GameControl.cs
@@ -11,15 +11,16 @@ public class GameControl : MonoBehaviour {
 	int PlayerNum = 0;
 	int[] Toggles = new int[20];
 	public bool CanMove = true;
+	public string GameStage;
 	//预设体
 	public GameObject PlayerCard, PlayerName;
 	//全局对象
-	public GameObject ConfigUI,PlayerUI,PlayerNow;
+	public GameObject ConfigUI,PlayerUI,PlayerNow,GameStatus;
 
 	//局部变量
 	GameObject[] Player = new GameObject[20];
 	string[] PlayerRole = new string[20];
-	string[] RoleList = {"狼人"};
+	string[] RoleList = {"狼人","狼人","狼人","狼人","先知","女巫","守卫","丘比特","猎人","长老","村民","村民","村民","村民","村民","村民","村民","村民"};
 
 	// Use this for initialization
 	void Start () {
@@ -27,6 +28,7 @@ public class GameControl : MonoBehaviour {
 		Toggles [0] = 0;
 		ConfigUI.SetActive (false);
 		PlayerUI.SetActive (false);
+		GameStage = "准备开始";
 	}
 	
 	// Update is called once per frame
@@ -80,24 +82,50 @@ public class GameControl : MonoBehaviour {
 	}
 	//开始
 	public void GameStart(){
+		if (PlayerNum < 1) {
+			GameStatus.GetComponent<Text>().text="压根没玩家啊！";
+			return;
+		}
 		if (Toggles [0] > PlayerNum) {
-			GameObject.Find ("MainCanvas/GameStatus").GetComponent<Text>().text="角色数量大于玩家数量";
+			GameStatus.GetComponent<Text>().text="角色数量大于玩家数量";
 			return;
 		}
 		if (Toggles [0] < PlayerNum) {
-			GameObject.Find ("MainCanvas/GameStatus").GetComponent<Text>().text="角色数量小于玩家数量";
+			GameStatus.GetComponent<Text>().text="角色数量小于玩家数量";
 			return;
 		}
-		GameObject.Find ("MainCanvas/GameStatus").GetComponent<Text>().text="游戏开始";
+		ConfigUI.SetActive (false);
+		GameStatus.GetComponent<Text>().text="游戏开始";
+		//根据配置获得角色列表
 		int i, j;
 		j = 0;
 		for (i = 1; i <= 18; i++) {
 			if (Toggles [i] == 1) {
-				PlayerRole [j] = RoleList [i];
+				PlayerRole [j] = RoleList [i-1];
 				j++;
 			}
-				
 		}
+		//随机给每个玩家分配角色
+		int rand;
+		for(i=0;i<PlayerNum;i++){
+			rand = Random.Range (0, PlayerNum - i);
+			Player[i].GetComponent<PlayerCard> ().Role = PlayerRole [rand];
+			for(j=rand;j<PlayerNum-1;j++){
+				PlayerRole[j]=PlayerRole[j+1];
+			}
+		}
+		GameStatus.GetComponent<Text>().text="请点击自己的卡牌查看身份";
+		GameStage = "查看身份";
 	}
 
+	public void NextStage(){
+		//查看身份-》第一夜
+		if(GameStage=="查看身份"){
+
+			GameStatus.GetComponent<Text>().text="天黑请闭眼";
+
+
+			GameStage = "第一夜";
+		}
+	}
 }
--- Assets/Script/PlayerCard.cs
+++ Assets/Script/PlayerCard.cs
@@ -6,6 +6,7 @@ public class PlayerCard : MonoBehaviour {
 
 	GameObject GameControl;
 	public GameObject PlayerName;
+	public string Role;
 
 	// Use this for initialization
 	void Start () {
@@ -26,24 +27,38 @@ public class PlayerCard : MonoBehaviour {
     IEnumerator OnMouseDown()  
     {  
 		Vector3 OldMousePosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		GameControl.GetComponent<GameControl> ().PlayerNow = this.gameObject;
 
-        //如果处于移动玩家状态
-		if (GameControl.GetComponent<GameControl> ().CanMove)
-	        //当鼠标左键按下时  
-	        while (Input.GetMouseButton (0)) {  
-				
-				Vector3 offset = Camera.main.ScreenToWorldPoint (Input.mousePosition) - OldMousePosition;
-				OldMousePosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
-				Vector3 NewPosition = transform.position + offset; 
-				GetComponent<Rigidbody2D> ().MovePosition (NewPosition);
-				PlayerName.GetComponent<Rigidbody2D> ().MovePosition (Camera.main.WorldToScreenPoint (NewPosition));
+		//如果游戏处于准备阶段
+		if (GameControl.GetComponent<GameControl> ().GameStage == "准备开始") {
+			//如果处于移动玩家状态
+			if (GameControl.GetComponent<GameControl> ().CanMove)
+				//当鼠标左键按下时  
+				while (Input.GetMouseButton (0)) {  
+
+					Vector3 offset = Camera.main.ScreenToWorldPoint (Input.mousePosition) - OldMousePosition;
+					OldMousePosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+					Vector3 NewPosition = transform.position + offset; 
+					GetComponent<Rigidbody2D> ().MovePosition (NewPosition);
+					PlayerName.GetComponent<Rigidbody2D> ().MovePosition (Camera.main.WorldToScreenPoint (NewPosition));
+					yield return new WaitForFixedUpdate ();  
+				}
+			//如果处于非移动状态
+			else {
+				GameControl.GetComponent<GameControl> ().PlayerUI.SetActive (true);
+			} 
+		}
+
+		//如果游戏处于查看身份阶段
+		if (GameControl.GetComponent<GameControl> ().GameStage == "查看身份") {
+			while (Input.GetMouseButton (0))
+			{
+				GameObject.Find ("MainCanvas/GameStatus").GetComponent<Text>().text=PlayerName.GetComponent<Text>().text+"："+Role;
 				yield return new WaitForFixedUpdate ();  
 			}
-		//如果处于非移动状态
-		else {
-			GameControl.GetComponent<GameControl> ().PlayerUI.SetActive (true);
-			GameControl.GetComponent<GameControl> ().PlayerNow = this.gameObject;
-		} 
+			GameObject.Find ("MainCanvas/GameStatus").GetComponent<Text>().text="请点击自己的卡牌查看身份";
+		}
+        
     }  
 
 	public void Delete(){
